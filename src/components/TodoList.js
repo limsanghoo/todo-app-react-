62c11f952b453bbc20668b5eb6c2e2b9f79e24c5
shlim <shlim@bizflow.com>
2021-08-01 21:49:22 +0900

    git commit after using react-virtualized.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
+import {List} from "react-virtualized";
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+    const rowRenderer = useCallback(
+        ({ index, key, style }) => {
+            const todo = todos[index];
+            return (
+                <TodoListItem
+                    todo={todo}
+                    key ={key}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                    style={style}
+                />
+            );
+        },
+        [onRemove, onToggle, todos]
+    )
     return (
-        <div className="TodoList">
+/*        <div className="TodoList">
             {todos.map(todo => (
                 <TodoListItem
                     todo={todo}
@@ -13,7 +29,17 @@ const TodoList = ({todos, onRemove, onToggle}) => {
                     onToggle={onToggle}
                 />
             ))}
-        </div>
+        </div>*/
+        <List
+            className="TodoList"
+            width={512}
+            height={513}
+            rowCount={todos.length}
+            rowHeight={57}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{outline: 'none'}}
+        />
     );
 };
 
